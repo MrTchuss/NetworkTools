48c0d348402ed48dc978790b18a85ebcf385b76a
Nicolas <nicolas@obelix.biscos.fr>
2012-07-01 13:08:06 +0200

    Added Thread workers to improve speed. Refactor snmp_login class structure.

--- snmp_login.py
+++ snmp_login.py
@@ -69,11 +69,11 @@ class Snmp:
          self.timeout = timeout;
 
    """
-   Generates a randome community name
+   Generates a random community name
    @return a random 8-bytes string
    """
    def randomCommunity(self):
-         self.community = ''.join([random.choice(string.ascii_uppercase) for x in range(8)])
+         self.community = ''.join([random.choice(string.ascii_uppercase) for x in xrange(8)])
 
    """
    Generates a random non-privileged port number
@@ -157,7 +157,7 @@ It can be proceed by two different functions (checkCommunityString
 and checkCommunityType, that respectiveley check whether the community
 name exists, and checks if the community name is RO or RW
 """
-class Worker(Thread):
+class AbstractWorker(Thread):
    def __init__(self, queue):
       Thread.__init__(self);
       self.__queue = queue;
@@ -165,21 +165,30 @@ class Worker(Thread):
       self.__doStop = False;
 
    """
-   Select the SNMP function processing
-   @param function One of checkCommunityString or checkCommunityType
+   To be extended in subclasses
    """
-   def setFunction(self, function):
-      if( 'checkCommunityString' == function ):
-         self.__function = self.checkCommunityString;
-      elif( 'checkCommunityType' == function ):
-         self.__function = self.checkCommunityType;
+   def _compute(self, snmp):
+      pass
 
    """
-   Set the shared thread lock
-   @param lock lock object
+   Thread main worker method. Pick up an snmp prepared packed in the Queue and process it
    """
-   def setLock(self, lock):
-      self.__lock = lock;
+   def run(self):
+      self.__doStop = False;
+      while not self.__doStop:
+         snmp = self.__queue.get()
+         self._compute(snmp)
+         self.__queue.task_done()
+
+   """
+   Stop the main worker method. Not sure if it is usefull here...
+   """
+   def stop(self):
+      self.__doStop = True;
+
+class CheckCommunityStringWorker(AbstractWorker):
+   def __init__(self, queue):
+      AbstractWorker.__init__(self, queue);
 
    """
    Set the shared SNMPResults object
@@ -192,21 +201,23 @@ class Worker(Thread):
    Check if the SNMP server responds to the SNMP object 
    @param snmp a prepared snmp packet
    """
-   def checkCommunityString(self, snmp):
+   def _compute(self, snmp):
       if( snmp.get(versionOid) and 0 == snmp.error() ):
          version = '1'
          if snmp.version == 1:
             version = '2c'
          print '[+] Community %s, version %s, for %s: "%s"' % (snmp.community, version, snmp.dst, snmp.response());
-         self.__lock.acquire()
          self.__results.add(snmp.dst, snmp.version, snmp.community);
-         self.__lock.release()
+
+class CheckCommunityTypeWorker(AbstractWorker):
+   def __init__(self, queue):
+      AbstractWorker.__init__(self, queue);
 
    """
    Check if the SNMP server responds to the SNMP object in RW or RO mode
    @param snmp a prepared snmp packet
    """
-   def checkCommunityType(self, snmp):
+   def _compute(self, snmp):
       if( snmp.get(nameOid) and not snmp.error() ):
          name = snmp.response();
          if( snmp.set(nameOid, name) ):
@@ -217,25 +228,11 @@ class Worker(Thread):
                access = 'RW';
             else:
                access = 'RO';
+            community = snmp.community
+            dst = snmp.dst
             print '[+] Community %s, version %s, for %s is %s' % (community, version, dst, access);
          else:
             print '[!] Something went wrong...'
-
-   """
-   Thread main worker method. Pick up an snmp prepared packed in the Queue and process it
-   """
-   def run(self):
-      self.__doStop = False;
-      while not self.__doStop:
-         snmp = self.__queue.get()
-         self.__function(snmp)
-         self.__queue.task_done()
-
-   """
-   Stop the main worker method. Not sure if it is usefull here...
-   """
-   def stop(self):
-      self.__doStop = True;
 
 """
 Simple container for SNMP check results
@@ -252,12 +249,15 @@ class SnmpResult:
 Container for multiple SNMP Results. Can be iterated
 """
 class SnmpResults:
-   def __init__(self):
+   def __init__(self, lock):
       self.__rslt = [];
+      self.__lock = lock;
 
    def add(self, dst, snmpVersion, communityName):
       snmpResult = SnmpResult(dst, snmpVersion, communityName);
+      self.__lock.acquire();
       self.__rslt.append(snmpResult);
+      self.__lock.release();
 
    def __iter__(self):
       self.__iter = self.__rslt.__iter__();
@@ -267,13 +267,17 @@ class SnmpResults:
       snmpResult = self.__iter.next();
       return snmpResult.get();
 
-def doHelp():
-   # To add when file support will be implemented
-   #Syntax: snmp_login.py [-C communityFileName] [-c communityName] [-d dport] [-s sport] [-o outputFile] [-v snmpVersion] [-w workers] [-t timeout] [-h] target1 [target2 [...]]
-   #   -o outputFileName
-   #   --output             write results in outputFileName
+class Main():
+   def __init__(self):
+      pass
 
-   print """
+   def doHelp(self):
+      # To add when file support will be implemented
+      #Syntax: snmp_login.py [-C communityFileName] [-c communityName] [-d dport] [-s sport] [-o outputFile] [-v snmpVersion] [-w workers] [-t timeout] [-h] target1 [target2 [...]]
+      #   -o outputFileName
+      #   --output             write results in outputFileName
+   
+      print """
 Scapy Script to brute force SNMP community names.
 The principle is taken from snmp_login Metasploit module. First, a set of SNMP GET request using versionOID is performed to capture the community names.
 Then, based on the results, these community names are used to read, then write back the nameOID. Based on the response status, the community is marked as RW or RO.
@@ -308,135 +312,136 @@ Advanced options:
 
 """
 
-"""
-"""
-def parseArgs():
-   communityList = ['public', 'private']
-   output        = sys.stdout;
-   stopOnFirst   = False
-   versionList   = [0, 1]
-   dport = 161
-   sport = None
-   workersCount=30
-   timeout = 0.5;
-   if( 'posix' != os.name ):
-      print '[!] Only on linux or unix system'
-      sys.exit(-1)
-   if( 0 != os.geteuid() ):
-      print '[!] Must be root'
-      sys.exit(-1);
-   try:
-      #opts, targetList = getopt(sys.argv[1:], 'c:C:d:s:o:v:w:h', ['community=', 'communityFile=', 'dport=', 'sport=', 'output=', 'help', 'version=']);
-      opts, targetList = getopt(sys.argv[1:], 'c:C:d:s:v:t:w:h', ['community=', 'communityFile=', 'dport=', 'sport=', 'help', 'version=']);
-   except GetoptError, e:
-      print '[!] %s' % str(e);
-      sys.exit(-1);
-  
-   for k, v in opts:
-      if( '-c' == k or '--community' == k ):
-         communityList = v.split(',');
-      elif( '-C' == k or '--communityFile' == k ):
-         if( not os.path.exists(v) ):
-            print '%s: no such file or directory' % (v)
-            sys.exit(-1)
-         communityList = open(v, 'r').read(-1).split('\n');
-      elif( '-d' == k or '--dport' == k ):
-         dport = int(v);
-      elif( '-s' == k or '--sport' == k ):
-         sport = int(v)
-      #elif( '-o' == k or '--output' == k ):
-      #   if( os.path.exists(v) ):
-      #      print '%s: file exists. Please use a different file name.' % (v)
-      #      sys.exit(-1);
-      #   output = open(v, 'w');
-      elif( '-v' == k or '--version' == k ):
-         for version in v.split(','):
-            if '1' == version:
-               versionList.append(0);
-            elif '2c' == version or '2C' == version:
-               versionList.append(1);
-            else:
-               print '[!] Unknown version %s' % (version)
-               sys.exit(-1);
-      elif( '-t' == k ):
-         timeout = float(v);
-      elif( '-w' == k ):
-         workersCount = int(v);
-      elif( '-h' == k or '--help' == k ):
-         doHelp();
-         sys.exit(0)
-
-   if( 0 == len(targetList) ):
-      print '[!] Must define at least one target'
-      sys.exit(-1);
+   """
+   """
+   def parseArgs(self):
+      self.communityList = ['public', 'private']
+      self.output        = sys.stdout;
+      self.stopOnFirst   = False
+      self.versionList   = [0, 1]
+      self.dport = 161
+      self.sport = None
+      self.workersCount=30
+      self.timeout = 0.5;
+      if( 'posix' != os.name ):
+         print '[!] Only on linux or unix system'
+         sys.exit(-1)
+      if( 0 != os.geteuid() ):
+         print '[!] Must be root'
+         sys.exit(-1);
+      try:
+         #opts, self.targetList = getopt(sys.argv[1:], 'c:C:d:s:o:v:w:h', ['community=', 'communityFile=', 'dport=', 'sport=', 'output=', 'help', 'version=']);
+         opts, self.targetList = getopt(sys.argv[1:], 'c:C:d:s:v:t:w:h', ['community=', 'communityFile=', 'dport=', 'sport=', 'help', 'version=']);
+      except GetoptError, e:
+         print '[!] %s' % str(e);
+         sys.exit(-1);
+     
+      for k, v in opts:
+         if( '-c' == k or '--community' == k ):
+            self.communityList = v.split(',');
+         elif( '-C' == k or '--communityFile' == k ):
+            if( not os.path.exists(v) ):
+               print '%s: no such file or directory' % (v)
+               sys.exit(-1)
+            self.communityList = open(v, 'r').read(-1).split('\n');
+         elif( '-d' == k or '--dport' == k ):
+            self.dport = int(v);
+         elif( '-s' == k or '--sport' == k ):
+            self.sport = int(v)
+         #elif( '-o' == k or '--output' == k ):
+         #   if( os.path.exists(v) ):
+         #      print '%s: file exists. Please use a different file name.' % (v)
+         #      sys.exit(-1);
+         #   self.output = open(v, 'w');
+         elif( '-v' == k or '--version' == k ):
+            for version in v.split(','):
+               if '1' == version:
+                  self.versionList.append(0);
+               elif '2c' == version or '2C' == version:
+                  self.versionList.append(1);
+               else:
+                  print '[!] Unknown version %s' % (version)
+                  sys.exit(-1);
+         elif( '-t' == k ):
+            self.timeout = float(v);
+         elif( '-w' == k ):
+            self.workersCount = int(v);
+         elif( '-h' == k or '--help' == k ):
+            self.doHelp();
+            sys.exit(0)
    
-   return targetList, communityList, output, stopOnFirst, dport, sport, versionList, workersCount, timeout;
-
-if( "__main__" == __name__ ):
-   # Retrieve arguments from command line
-   targetList, communityList, output, stopOnFirst, dport, sport, versionList, workersCount, timeout =  parseArgs()
-
-   # Creates the sync Queues
-   q = Queue()
-
-   # Creates a list of workerss
-   workerList = []
-   lock = Lock();
-   results = SnmpResults()
-   for worker in range(workersCount):
-      worker = Worker(q);
-      worker.setFunction("checkCommunityString")
-      worker.setResults(results);
-      worker.setLock(lock);
-      workerList.append(worker);
-      worker.start();
-
-   # Fills the synchronized queue with SNMP prepared packets
-   for community in communityList:
-      for dst in targetList:
-         for version in versionList:
-            snmp = Snmp()
-            snmp.dport = dport;
-            snmp.sport = sport;
-            snmp.timeout=timeout
-            snmp.dst = dst;
-            snmp.version = version;
-            snmp.community = community;
-            q.put(snmp);
-   # Wait for workers to end
-   q.join();
+      if( 0 == len(self.targetList) ):
+         print '[!] Must define at least one target'
+         sys.exit(-1);
    
-   # Not sure if it is useful, but stops the workers
-   for worker in workerList:
-      worker.stop();
-
-   # Initialize new workers for more community type-check work
-   print ''
-   q = Queue()
-   workerList = []
-   for worker in range(workersCount):
-      worker = Worker(q);
-      worker.setFunction("checkCommunityType")
-      worker.setResults(None);
-      worker.setLock(None);
-      workerList.append(worker);
-      worker.start();
- 
-   # Retrieve previously discovered snmp communities and check their types
-   for dst, community, version in results:
+   """
+   Creates a SNMP packet
+   """
+   def __createSNMPPacket(self, dst, version, community):
       snmp = Snmp()
-      snmp.dport = dport;
-      snmp.sport = sport;
-      snmp.timeout=timeout
-      snmp.dst = dst;
+      snmp.dport   = self.dport;
+      snmp.sport   = self.sport;
+      snmp.timeout = self.timeout
+      snmp.dst     = dst;
       snmp.version = version;
       snmp.community = community;
-      q.put(snmp);
-   # Wait for workers to end
-   q.join();
+      return snmp
 
-   # Not sure if it is useful, but stops the workers
-   for worker in workerList:
-      worker.stop();
+   """
+   Go, go, go !
+   """
+   def run(self):
+      # Retrieve arguments from command line
+      self.parseArgs()
+   
+      # Creates the sync Queues
+      q = Queue()
+   
+      # Creates a lock
+      lock = Lock();
 
+      # Creates a list of workerss
+      workerList = []
+      results = SnmpResults(lock)
+      for worker in xrange(self.workersCount):
+         worker = CheckCommunityStringWorker(q);
+         worker.setResults(results);
+         workerList.append(worker);
+         worker.start();
+   
+      # Fills the synchronized queue with SNMP prepared packets
+      for community in self.communityList:
+         for dst in self.targetList:
+            for version in self.versionList:
+               snmp = self.__createSNMPPacket(dst, version, community);
+               q.put(snmp);
+      # Wait for workers to end
+      q.join();
+      
+      # Not sure if it is useful, but stops the workers
+      for worker in workerList:
+         worker.stop();
+   
+      # Initialize new workers for more community type-check work
+      print ''
+      q = Queue()
+      workerList = []
+      for worker in xrange(self.workersCount):
+         worker = CheckCommunityTypeWorker(q);
+         workerList.append(worker);
+         worker.start();
+    
+      # Retrieve previously discovered snmp communities and check their types
+      for dst, community, version in results:
+         snmp = self.__createSNMPPacket(dst, version, community);
+         q.put(snmp);
+      # Wait for workers to end
+      q.join();
+   
+      # Not sure if it is useful, but stops the workers
+      for worker in workerList:
+         worker.stop();
+   
+if( '__main__' == __name__ ):
+   Main().run();
 
